1ad2d1ef64c31b63f6592c9f016a55c9b531ba9f
mab6428 <mab6428@thi.de>
2021-11-19 22:21:04 +0000

    Added createTxSetupUpdate
    - Added createTxSetupUpdate function

--- src/CANFD_BCM_Example.c
+++ src/CANFD_BCM_Example.c
@@ -377,6 +377,97 @@ void createTxSetupSequence(int const *const socketFD, struct canfd_frame frames[
 }
 
 /**
+ * Updates a cyclic transmission task for one or multiple CAN/CANFD frames.
+ *
+ * @param socketFD - The socket file descriptor.
+ * @param frames   - The array of CAN/CANFD frames with the updated data.
+ * @param nframes  - The number of CAN/CANFD frames that should be updated.
+ * @param isCANFD  - Flag for CANFD frames.
+ * @param announce - The cycle is retained but the changed data will be send immediately once.
+ */
+void createTxSetupUpdate(int const *const socketFD, struct canfd_frame frames[], int nframes, int isCANFD, int announce){
+
+    // BCM message we are sending with multiple CAN or CANFD frame
+    void* msg      = NULL;
+    size_t msgSize = 0;
+
+    // Check if we are sending CAN or CANFD frames.
+    if(isCANFD){
+        msgSize = sizeof(struct bcmMsgSingleFrameCanFD);
+        msg = malloc(msgSize);
+    }else{
+        msgSize = sizeof(struct bcmMsgSingleFrameCan);
+        msg = malloc(msgSize);
+    }
+
+    // Error handling
+    if(msg == NULL){
+        printf("Error could not allocate memory for the message \n");
+        shutdownHandler(ERR_MALLOC_FAILED, socketFD);
+    }
+
+    // Note: Always initialize the whole struct with 0.
+    // Random values in the memory can cause weird bugs!
+    memset(msg, 0, msgSize);
+
+    // Note: By combining the flags SETTIMER and STARTTIMER
+    // the BCM will start sending the messages immediately
+    if(isCANFD){
+        struct bcmMsgSingleFrameCanFD *msgCANFD = (struct bcmMsgSingleFrameCanFD *) msg;
+
+        msgCANFD->msg_head.opcode  = TX_SETUP;
+        msgCANFD->msg_head.flags   = CAN_FD_FRAME;
+        msgCANFD->msg_head.nframes = 1;
+
+        if(announce){
+            msgCANFD->msg_head.flags = msgCANFD->msg_head.flags | TX_ANNOUNCE;
+        }
+
+    }else{
+        struct bcmMsgSingleFrameCan *msgCAN = (struct bcmMsgSingleFrameCan *) msg;
+
+        msgCAN->msg_head.opcode    = TX_SETUP;
+        msgCAN->msg_head.nframes   = 1;
+
+        if(announce){
+            msgCAN->msg_head.flags = TX_ANNOUNCE;
+        }
+
+    }
+
+    for(int index = 0; index < nframes; index++){
+
+        // Fill out the CAN ID and frame data
+        if(isCANFD){
+            struct bcmMsgSingleFrameCanFD *msgCANFD = (struct bcmMsgSingleFrameCanFD *) msg;
+            struct canfd_frame *canfdFrame = &frames[index];
+
+            msgCANFD->msg_head.can_id = canfdFrame->can_id;
+            msgCANFD->canfdFrame[0]   = *canfdFrame;
+
+        }else{
+            struct bcmMsgSingleFrameCan *msgCAN = (struct bcmMsgSingleFrameCan *) msg;
+            struct can_frame *canFrame = (struct can_frame*) &frames[index];
+
+            msgCAN->msg_head.can_id = canFrame->can_id;
+            msgCAN->canFrame[0]     = *canFrame;
+        }
+
+        // Send the TX_SETUP configuration message
+        if(send(*socketFD, msg, msgSize, 0) < 0){
+            printf("Error could not send TX_SETUP message \n");
+            shutdownHandler(ERR_TX_SETUP_FAILED, socketFD);
+        }
+
+    }
+
+    // Free the allocated memory for the BCM message
+    if(msg != NULL){
+        free(msg);
+    }
+}
+
+/**
  * Removes a cyclic transmission task for a CAN ID.
  *
  * Note: If the cyclic transmission task was created with createTxSetupSequence
@@ -652,65 +743,86 @@ int main(){
     printf("Setup the socket on the interface %s\n", INTERFACE);
 
     // Test CAN Frame
-    struct can_frame frame1;
-    frame1.can_id  = 0x123;
-    frame1.can_dlc = 4;
-    frame1.data[0] = 0xDE;
-    frame1.data[1] = 0xAD;
-    frame1.data[2] = 0xBE;
-    frame1.data[3] = 0xEF;
+    struct can_frame canFrame1;
+    canFrame1.can_id  = 0x123;
+    canFrame1.can_dlc = 4;
+    canFrame1.data[0] = 0xDE;
+    canFrame1.data[1] = 0xAD;
+    canFrame1.data[2] = 0xBE;
+    canFrame1.data[3] = 0xEF;
 
-    struct can_frame frame2;
-    frame2.can_id  = 0x345;
-    frame2.can_dlc = 3;
-    frame2.data[0] = 0xC0;
-    frame2.data[1] = 0xFF;
-    frame2.data[2] = 0xEE;
+    struct can_frame canFrame2;
+    canFrame2.can_id  = 0x345;
+    canFrame2.can_dlc = 3;
+    canFrame2.data[0] = 0xC0;
+    canFrame2.data[1] = 0xFF;
+    canFrame2.data[2] = 0xEE;
 
     struct canfd_frame frameArrCAN[2];
-    frameArrCAN[0] = *((struct canfd_frame*) &frame1);
-    frameArrCAN[1] = *((struct canfd_frame*) &frame2);
+    frameArrCAN[0] = *((struct canfd_frame*) &canFrame1);
+    frameArrCAN[1] = *((struct canfd_frame*) &canFrame2);
+
+    struct can_frame canFrame1Modified;
+    canFrame1Modified.can_id  = 0x123;
+    canFrame1Modified.can_dlc = 4;
+    canFrame1Modified.data[0] = 0xBE;
+    canFrame1Modified.data[1] = 0xEF;
+    canFrame1Modified.data[2] = 0xDE;
+    canFrame1Modified.data[3] = 0xAD;
+
+    struct can_frame canFrame2Modified;
+    canFrame2Modified.can_id  = 0x345;
+    canFrame2Modified.can_dlc = 5;
+    canFrame2Modified.data[0] = 0xC0;
+    canFrame2Modified.data[1] = 0xFF;
+    canFrame2Modified.data[2] = 0xEE;
+    canFrame2Modified.data[3] = 0xFF;
+    canFrame2Modified.data[4] = 0xEE;
+
+    struct canfd_frame frameArrCANModified[2];
+    frameArrCANModified[0] = *((struct canfd_frame*) &canFrame1Modified);
+    frameArrCANModified[1] = *((struct canfd_frame*) &canFrame2Modified);
 
     // Test CANFD Frame
-    struct canfd_frame frame3;
-    frame3.can_id   = 0x567;
-    frame3.len      = 16;
-    frame3.data[0]  = 0xDE;
-    frame3.data[1]  = 0xAD;
-    frame3.data[2]  = 0xBE;
-    frame3.data[3]  = 0xEF;
-    frame3.data[4]  = 0xDE;
-    frame3.data[5]  = 0xAD;
-    frame3.data[6]  = 0xBE;
-    frame3.data[7]  = 0xEF;
-    frame3.data[8]  = 0xDE;
-    frame3.data[9]  = 0xAD;
-    frame3.data[10] = 0xBE;
-    frame3.data[11] = 0xEF;
-    frame3.data[12] = 0xDE;
-    frame3.data[13] = 0xAD;
-    frame3.data[14] = 0xBE;
-    frame3.data[15] = 0xEF;
+    struct canfd_frame canfdFrame1;
+    canfdFrame1.can_id   = 0x567;
+    canfdFrame1.len      = 16;
+    canfdFrame1.data[0]  = 0xDE;
+    canfdFrame1.data[1]  = 0xAD;
+    canfdFrame1.data[2]  = 0xBE;
+    canfdFrame1.data[3]  = 0xEF;
+    canfdFrame1.data[4]  = 0xDE;
+    canfdFrame1.data[5]  = 0xAD;
+    canfdFrame1.data[6]  = 0xBE;
+    canfdFrame1.data[7]  = 0xEF;
+    canfdFrame1.data[8]  = 0xDE;
+    canfdFrame1.data[9]  = 0xAD;
+    canfdFrame1.data[10] = 0xBE;
+    canfdFrame1.data[11] = 0xEF;
+    canfdFrame1.data[12] = 0xDE;
+    canfdFrame1.data[13] = 0xAD;
+    canfdFrame1.data[14] = 0xBE;
+    canfdFrame1.data[15] = 0xEF;
 
-    struct canfd_frame frame4;
-    frame4.can_id   = 0x789;
-    frame4.len      = 12;
-    frame4.data[0]  = 0xC0;
-    frame4.data[1]  = 0xFF;
-    frame4.data[2]  = 0xEE;
-    frame4.data[3]  = 0xC0;
-    frame4.data[4]  = 0xFF;
-    frame4.data[5]  = 0xEE;
-    frame4.data[6]  = 0xC0;
-    frame4.data[7]  = 0xFF;
-    frame4.data[8]  = 0xEE;
-    frame4.data[9]  = 0xC0;
-    frame4.data[10] = 0xFF;
-    frame4.data[11] = 0xEE;
+    struct canfd_frame canfdFrame2;
+    canfdFrame2.can_id   = 0x789;
+    canfdFrame2.len      = 12;
+    canfdFrame2.data[0]  = 0xC0;
+    canfdFrame2.data[1]  = 0xFF;
+    canfdFrame2.data[2]  = 0xEE;
+    canfdFrame2.data[3]  = 0xC0;
+    canfdFrame2.data[4]  = 0xFF;
+    canfdFrame2.data[5]  = 0xEE;
+    canfdFrame2.data[6]  = 0xC0;
+    canfdFrame2.data[7]  = 0xFF;
+    canfdFrame2.data[8]  = 0xEE;
+    canfdFrame2.data[9]  = 0xC0;
+    canfdFrame2.data[10] = 0xFF;
+    canfdFrame2.data[11] = 0xEE;
 
     struct canfd_frame frameArrCANFD[2];
-    frameArrCANFD[0] = frame3;
-    frameArrCANFD[1] = frame4;
+    frameArrCANFD[0] = canfdFrame1;
+    frameArrCANFD[1] = canfdFrame2;
 
     // Test intervals
     struct bcm_timeval ival1;
@@ -718,12 +830,20 @@ int main(){
     ival1.tv_usec = 500;
 
     struct bcm_timeval ival2;
-    ival2.tv_sec  = 1;
+    ival2.tv_sec  = 3;
     ival2.tv_usec = 0;
+
+    struct bcm_timeval ivalZero;
+    ivalZero.tv_sec  = 0;
+    ivalZero.tv_usec = 0;
 
     struct bcm_timeval ivalArr1[2];
     ivalArr1[0] = ival1;
     ivalArr1[1] = ival1;
+
+    struct bcm_timeval ivalArr1Zero[2];
+    ivalArr1Zero[0] = ivalZero;
+    ivalArr1Zero[1] = ivalZero;
 
     struct bcm_timeval ivalArr2[2];
     ivalArr2[0] = ival2;
@@ -733,6 +853,10 @@ int main(){
     uint32_t countArr[2];
     countArr[0] = 10;
     countArr[1] = 5;
+
+    uint32_t countArrZero[2];
+    countArr[0] = 0;
+    countArr[1] = 0;
 
     // Test Mask
     struct canfd_frame mask;
@@ -757,15 +881,27 @@ int main(){
     //createTxSetupSequence(&socketFD, frameArrCANFD, 2, 10, ival1, ival2, 1);
     //sleep(10);
 
+    // TX_SETUP Announce Test without announce
+    //createTxSetup(&socketFD, frameArrCAN, 2, countArrZero, ivalArr1Zero, ivalArr2,0);
+    //sleep(10);
+    //createTxSetupUpdate(&socketFD, frameArrCANModified, 2, 0, 0);
+    //sleep(10);
+
+    // TX_SETUP Announce Test with announce
+    //createTxSetup(&socketFD, frameArrCAN, 2, countArrZero, ivalArr1Zero, ivalArr2,0);
+    //sleep(10);
+    //createTxSetupUpdate(&socketFD, frameArrCANModified, 2, 0, 1);
+    //sleep(10);
+
     // TX_DELETE Test
-    //createTxSetupSequence(&socketFD, &frame3, 1, 10, ival1, ival2, 1);
+    //createTxSetupSequence(&socketFD, &canfdFrame1, 1, 10, ival1, ival2, 1);
     //sleep(5);
-    //createTxDelete(&socketFD, frame3.can_id, 1);
+    //createTxDelete(&socketFD, canfdFrame1.can_id, 1);
     //sleep(10);
 
     //createTxSetupSequence(&socketFD, frameArrCANFD, 2, 10, ival1, ival2, 1);
     //sleep(5);
-    //createTxDelete(&socketFD, frame3.can_id, 1);
+    //createTxDelete(&socketFD, canfdFrame1.can_id, 1);
     //sleep(10);
 
     // RX_SETUP CAN ID Test
